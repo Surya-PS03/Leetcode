3ac596747eb11dfa88eda09e71abd67c857d742b
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-07-27 00:49:01 +0530

    Time: 952 ms (44.62%), Space: 71.1 MB (38.46%) - LeetHub

--- 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.py
+++ 3185-count-pairs-that-form-a-complete-day-ii/3185-count-pairs-that-form-a-complete-day-ii.py
@@ -9,10 +9,10 @@ class Solution:
         hmap = defaultdict(int)
         count = 0
         for hr in hours:
-            x = (24-hr%24)%24
+            x = (24-hr%24)%24 #what the current number that is hr%24 needs
             if  x in hmap:
                 count += hmap[x]
             
-            hmap[hr%24]+=1
+            hmap[hr%24]+=1 # storing in map if some pair eg hr = 11 in future 13 appears so it finds 11
         
         return  count

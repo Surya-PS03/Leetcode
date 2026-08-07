e4a6e1054694ddeac761cd5a6e1392c343515bb3
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-08-07 13:48:02 +0530

    Time: 392 ms (60.46%), Space: 21 MB (77.98%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -6,25 +6,39 @@ class Solution:
 
         N = len(s)
 
-        dp = [[-1]*N for _ in range(N)]
+        # dp = [[-1]*N for _ in range(N)]
 
-        def solve(i,j):
+        # def solve(i,j):
 
-            if i>=N or j>=N:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
+        #     if i>=N or j>=N:
+        #         return 0
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
 
-            x,a,b = float("-inf"),float("-inf"),float("-inf")
+        #     x,a,b = float("-inf"),float("-inf"),float("-inf")
 
-            if s[i] == s2[j]:
-                x = 1 + solve(i+1,j+1)
-            else:
-                a = solve(i+1,j)
-                b = solve(i,j+1)
+        #     if s[i] == s2[j]:
+        #         x = 1 + solve(i+1,j+1)
+        #     else:
+        #         a = solve(i+1,j)
+        #         b = solve(i,j+1)
 
-            dp[i][j] = max(x,b,a) 
-            return dp[i][j]
+        #     dp[i][j] = max(x,b,a) 
+        #     return dp[i][j]
         
 
-        return N-solve(0,0)
+        # return N-solve(0,0)
+
+
+        # Bottom up
+        dp = [[0]*(N+1) for _ in range(N+1)]
+
+        for i in range(N-1,-1,-1):
+            for j in range(N-1,-1,-1):
+
+                if s[i]==s2[j]:
+                    dp[i][j] = 1+dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+        
+        return N-dp[0][0]

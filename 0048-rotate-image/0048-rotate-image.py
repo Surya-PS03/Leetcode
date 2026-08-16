5dfd8a6c06a2bb0e3d83df39c67f611f2c9c5936
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-08-16 21:23:23 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.81%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        # replace diagonals
+        N = len(matrix)
+        
+        for i in range(N):
+            for j in range(i+1):
+
+                if i==j:
+                    continue
+                
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        
+        print(matrix)
+        # reverse all rows
+
+        for i in range(N):
+
+            for j in range(N//2):
+
+                matrix[i][N-j-1],matrix[i][j] = matrix[i][j],matrix[i][N-j-1]
+        
+        

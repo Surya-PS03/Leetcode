153b9e60d176687182a98a27cc1b4756f67ed44e
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-08-18 00:35:14 +0530

    Time: 8 ms (49.02%), Space: 22.1 MB (94.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,12 +2,43 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         
-        freq = Counter(nums)
-        N = len(nums)
-        res = []
-        for num,fr in freq.items():
+        count1,count2 = 0,0
+        candidate1,candidate2 = None,None
 
-            if fr>N//3:
-                res.append(num)
+        for num in nums:
+
+            if candidate1==num:
+                count1+=1
+            elif candidate2==num:
+                count2+=1
+            elif count1==0:
+                candidate1 = num
+                count1 = 1
+            elif count2==0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1-=1
+                count2-=1
+        
+
+        count1 = 0
+        count2 = 0
+
+        for num in nums:
+
+            if num==candidate1:
+                count1+=1
+            elif num==candidate2:
+                count2+=1
+        
+
+
+        res = []
+
+        if count1>len(nums)//3:
+            res.append(candidate1)
+        if count2>len(nums)//3:
+            res.append(candidate2)
         
         return res

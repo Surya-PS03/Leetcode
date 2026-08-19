9472e84251e20daf03539717378dc9b17c9c6821
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-08-20 00:02:10 +0530

    Time: 0 ms (100%), Space: 19.2 MB (93.56%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -2,12 +2,15 @@ from functools import cache
 class Solution:
     def integerReplacement(self, n: int) -> int:
         
-        @cache
+        dp = {}
+
         def solve(n):
 
             if n==1:
                 return 0
-            
+            if n in dp:
+                return dp[n]
+
             even = float("inf")
             odd = float("inf")
 
@@ -18,6 +21,7 @@ class Solution:
                 minus_one = 1 + solve(n-1)
                 odd = min(plus_one,minus_one)
 
-            return min(even,odd)
+            dp[n] = min(even,odd)
+            return dp[n]
             
         return solve(n)

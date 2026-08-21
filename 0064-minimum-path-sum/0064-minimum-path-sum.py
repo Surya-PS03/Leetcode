54f6da672fc28038401a1c60bfd0c049f505344b
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-08-21 12:53:53 +0530

    Time: 46 ms (8.41%), Space: 22.1 MB (18.64%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -5,18 +5,38 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        @cache
-        def solve(i,j):
+        # @cache
+        # def solve(i,j):
 
-            if i == m-1 and j == n-1:
-                return grid[i][j]
-            elif i == m or j == n:
-                return float("inf")
-
-        
-            right = grid[i][j] + solve(i+1,j)
-            down = grid[i][j] + solve(i,j+1)
-            return min(right,down)
+        #     if i == m-1 and j == n-1:
+        #         return grid[i][j]
+        #     elif i == m or j == n:
+        #         return float("inf")
 
         
-        return solve(0,0)
+        #     right = grid[i][j] + solve(i+1,j)
+        #     down = grid[i][j] + solve(i,j+1)
+        #     return min(right,down)
+
+        
+        # return solve(0,0)
+
+
+        # tabulation
+        m,n = len(grid),len(grid[0])
+        dp = [[0]*n for _ in range(m)]
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+
+                if i == m-1 and j == n-1:
+                    dp[i][j] = grid[i][j]
+                elif i == m-1:
+                    dp[i][j] = grid[i][j] + dp[i][j+1]
+                elif j == n-1:
+                    dp[i][j] = grid[i][j] + dp[i+1][j]
+                else:
+                    print(i,j)
+                    dp[i][j] = min(grid[i][j] + dp[i+1][j], grid[i][j] + dp[i][j+1])
+        
+        return dp[0][0]

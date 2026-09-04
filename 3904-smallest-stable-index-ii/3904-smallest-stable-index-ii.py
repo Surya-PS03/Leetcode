1c220ce4f074986185ac35924d4046faf4341c59
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-09-04 18:50:53 +0530

    Time: 232 ms (30%), Space: 33.3 MB (14.09%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        
+        N = len(nums)
+        maxPref = [0]*N
+        minSuff = [0]*N
+
+        maxPref[0] = nums[0]
+        minSuff[-1] = nums[-1]
+
+        for i in range(1,N):
+            maxPref[i] = max(maxPref[i-1],nums[i])
+
+        for i in range(N-2,-1,-1):
+            minSuff[i] = min(minSuff[i+1],nums[i])
+
+        for i in range(N):
+            if maxPref[i]-minSuff[i]<=k:
+                return i
+        
+        return -1

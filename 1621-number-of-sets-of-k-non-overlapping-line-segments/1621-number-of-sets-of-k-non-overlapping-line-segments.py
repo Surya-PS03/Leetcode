f8e3c7bb69d9962cb8dd4dd64b9239ddb4224d89
Surya Pratap Singh <139345772+Surya-PS03@users.noreply.github.com>
2026-09-17 00:40:35 +0530

    Time: 702 ms (39.65%), Space: 42.8 MB (44.83%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,15 +1,55 @@
+from functools import lru_cache
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
-        @lru_cache(None)
-        def dp(i, k, isStart):
-            if k == 0: return 1 # Found a way to draw k valid segments
-            if i == n: return 0 # Reach end of points
-            ans = dp(i+1, k, isStart) # Skip ith point
-            if isStart:
-                ans += dp(i+1, k, False) # Take ith point as start
-            else:
-                ans += dp(i, k-1, True) # Take ith point as end
-            return ans % MOD
-        return dp(0, k, True)
         
+        N = n
+        MOD = 10**9 + 7
+
+        # @lru_cache(None)
+        # def solve(i,k):
+
+        #     if k==0:
+        #         return 1
+            
+        #     if i>=N:
+        #         return 0
+
+        #     # skip
+
+        #     skip = solve(i+1,k)%MOD
+
+        #     # take
+        #     take = 0
+        #     for j in range(i+1,N):
+        #         take = (take + solve(j,k-1))%MOD
+            
+        #     return (skip+take)%MOD
+
+        # return solve(0,k)
+
+
+        # # # BOTTOM UP
+
+
+        dp = [[0]*(N+1) for _ in range(k+1)]
+
+        # base case
+        for i in range(N):
+            dp[0][i] = 1
+
+        for K in range(1,k+1):
+
+            prev = [0]*(N+1)
+
+            for x in range(N-1,-1,-1):
+                prev[x] = prev[x+1] + dp[K-1][x]
+
+            for i in range(N-1,-1,-1):
+
+                take = prev[i+1]                
+                
+                skip = dp[K][i+1] % MOD
+
+                dp[K][i] = (skip+take)%MOD
+
+        return dp[k][0]
